c780a86628522a2ed1218d84967194ecd87721fe
MarioTriMar <mario.t.m2001@hotmail.com>
2025-01-31 19:55:27 +0100

    commit

---
+++ src/main/java/com/desarrollo/laboratorio/ms_book_payments/MsBookPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo.laboratorio.ms_book_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBookPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBookPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desarrollo/laboratorio/ms_book_payments/MsBookPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrollo.laboratorio.ms_book_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBookPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
